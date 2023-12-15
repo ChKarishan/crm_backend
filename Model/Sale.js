d00e76dd17597e552f19038c1cdeaa547a2c8e28
ChKarishan <karishan.897@gmail.com>
2023-12-16 00:59:37 +0500

    model and route correction

--- Model/Sale.js
+++ Model/Sale.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
 const saleSchema = new mongoose.Schema({
-  saleID: {type: String,required: true, unique: true },
+  // saleID: {type: String,required: true, unique: true },
   numberOfPanels: {type: Number,required: true},
   totalWattage: {type: Number, required: true},
   redline: {type: String},
   financingDetails: {type: String},
-  Price: Number,
-  Date: String,
+  Price: {type: Number},
+  Date: {type: String},
   agent: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
   installers: [{type: mongoose.Schema.Types.ObjectId, ref: 'Installer'}]
 });
